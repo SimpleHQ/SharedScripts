357cb9a0a4430297de9f5dab130046d41dee85ad
Guy Carpenter <guy.carpenter@simplehq.com.au>
2017-03-09 15:13:37 +1000

    Cleanup cov files before running coverage

--- cover.sh
+++ cover.sh
@@ -18,6 +18,7 @@ export PKGS_COV=$(go list -f '{{if (len .GoFiles)}}{{.ImportPath}}
 # For each package that contains tests, run that package's' tests and capture coverage for all packages. Rinse repeat.
 # Note: the Go coverage tool is geared towards measuring coverage of an individual package from its tests.
 # Also ensure we don't get clashes from identically named packages
+rm -f *.cov
 go list -f '{{if or (len .TestGoFiles) (len .XTestGoFiles)}}go test -covermode set -coverprofile {{.Name}}_{{len .Imports}}_{{len .Deps}}.cov -coverpkg $PKGS_COV {{.ImportPath}}{{end}}' $PKGS | xargs -I {} bash -c {}
 
 # Merge all the coverage files together
